612e6b86ebe328e8d77732f28f219c4ea11e35bd
Admin <nguy1104@msu.edu>
2025-03-29 20:25:23 -0400

    finished lab10.5

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import ItemList from './components/Itemlist'
+import './App.css'
+
+function App() {
+  const items = [
+    { id: 1, name: 'React Book', price: 29.99, inStock: true },
+    { id: 2, name: 'Vite Sticker', price: 2.99, inStock: false },
+    { id: 3, name: 'Netlify Hat', price: 19.99, inStock: true },
+  ]
+
+  return (
+    <div className="App">
+      <Header />
+      {items.length > 0 ? (
+        <ItemList items={items} />
+      ) : (
+        <p>No items available.</p>
+      )}
+      <Footer />
+    </div>
+  )
+}
+export default App
